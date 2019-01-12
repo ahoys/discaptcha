29dd7808a9a5633543042c736b4c4064205c46ce
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-12 23:26:59 +0200

    Added command leave.

--- src/commands/command.leave.js
+++ src/commands/command.leave.js
@@ -1,10 +1,37 @@
 const log = require('debug')('command.leave');
-const auth = require('../../configs/auth.json');
-const config = require('../../configs/config.json');
 
+/**
+ * Makes the bot leave the server.
+ */
 module.exports = (Client, Message) => {
   try {
-
+    if (
+      Message &&
+      Message.guild &&
+      Message.guild.name &&
+      Message.author &&
+      Message.author.username
+    ) {
+      const User = Message.author;
+      User
+        .createDM()
+        .then((DMChannel) => {
+          DMChannel
+            .send(
+              `Thank you for having me ${User.username}!\n\n` +
+              'If you had any issues with the bot, please leave a note here: ' +
+              'https://github.com/ahoys/discaptcha/issues'
+            )
+            .then(() => {
+              const Guild = Message.guild;
+              Guild
+                .leave()
+                .then(() => {
+                  log(`User ${User.username} asked me to leave ${Guild.name}.`);
+                });
+            });
+        });
+    }
   } catch (e) {
     log(e);
   }
