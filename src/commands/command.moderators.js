5ac5abf591cfb4576176277ac5f30b0b0a4003a1
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-12 23:50:25 +0200

    Added moderators.

--- src/commands/command.moderators.js
+++ src/commands/command.moderators.js
@@ -1,10 +1,37 @@
 const log = require('debug')('command.moderators');
-const auth = require('../../configs/auth.json');
 const config = require('../../configs/config.json');
 
+/**
+ * Prints moderator role information.
+ */
 module.exports = (Client, Message) => {
   try {
-
+    if (
+      Message &&
+      Message.guild &&
+      Message.guild.roles &&
+      Message.guild.id &&
+      Message.guild.members
+    ) {
+      const Guild = Message.guild;
+      const moderatorRole = Guild.roles
+        .find(r => String(r.id) === String(config.guilds[Guild.id].moderatorRoleId));
+      if (moderatorRole) {
+        const moderators = Message.guild.members
+          .filter(m => m.roles.find(r => r.id === moderatorRole.id) !== null);
+        if (moderators && moderators.size) {
+          Message.reply(
+            'The current moderator role is ' +
+            `"${moderatorRole.name}", id: ${moderatorRole.id}.\n\n` +
+            `There are ${moderators.size} moderators.`
+          );
+        } else {
+          Message.reply('No moderators set.');
+        }
+      } else {
+        Message.reply('Moderator role is not set.');
+      }
+    }
   } catch (e) {
     log(e);
   }
