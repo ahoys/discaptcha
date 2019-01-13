cd70ae6810682813a471b95b17a6e4b4135ed35d
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-13 19:44:09 +0200

    Greatly simplified unverified.

--- src/commands/command.unverified.js
+++ src/commands/command.unverified.js
@@ -1,46 +1,23 @@
+const guildUtil = require('../utilities/util.guild');
 const log = require('debug')('command.unverified');
-const config = require('../../configs/config.json');
 
 /**
  * Lists all clients that do not have the verification role.
  */
 module.exports = (Client, Message, value = '') => {
   try {
-    const { guild } = Message;
-    const { guilds } = config;
-    if (
-      guilds &&
-      guilds[guild.id] &&
-      guilds[guild.id].verificationRoleId
-    ) {
-      const roleId = guilds[guild.id].verificationRoleId;
-      const requestedRole = guild.roles.find(r => String(r.id) === String(roleId));
-      if (requestedRole) {
-        const missing = guild.members.filter(m => {
-          const result = m.roles.find(r => r.id === requestedRole.id);
-          return result === null;
+    const Role = guildUtil.getVerificationRoleOfGuild(Message.guild);
+    if (Role) {
+      guildUtil
+        .getGuildMembersWithOrWithoutRole(Message.guild, false, Role)
+        .then((GuildMembers) => {
+          const size = GuildMembers.size;
+          Message.reply(`There are ${size} client(s) without the ${Role.name} -role.`);
+        })
+        .catch((e) => {
+          log(e);
+          Message.reply('execution failed.');
         });
-        if (missing && missing.size) {
-          let i = 0;
-          let msg = '';
-          missing.forEach((member) => {
-            if (i === 0) {
-              msg += `${member.user.username}`;
-            } else {
-              msg += `, ${member.user.username}`;
-            }
-            i += 1;
-          });
-          Message.reply(`The following members do not have the role: ${msg}.`);
-          Message.reply('Maybe you should try the command "humanize"?');
-        } else {
-          Message.reply('Everyone got the role.');
-        }
-      } else {
-        Message.reply('The verification role was not found. Does it exist?');
-      }
-    } else {
-      Message.reply('The verification role is missing from the config.');
     }
   } catch (e) {
     log(e);
