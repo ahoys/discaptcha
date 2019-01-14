4f4da3d0b46eade96fc4f97f988c4805cfd19361
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-14 21:14:13 +0200

    Added command role execution code.

--- src/commands/command.role.js
+++ src/commands/command.role.js
@@ -1,9 +1,19 @@
+const guildUtil = require('../utilities/util.guild');
 const log = require('debug')('command.role');
-const auth = require('../../configs/auth.json');
-const config = require('../../configs/config.json');
 
 module.exports = (Client, Message, value = '') => {
   try {
+    const Role = guildUtil.getVerificationRoleOfGuild(Message.guild);
+    if (Role) {
+      Message.reply(
+        `The current verification role is \`${Role.name}\`, id \`${Role.id}\`.`
+      );
+    } else {
+      Message.reply(
+        'The verification role not set.' +
+          'See help for a command on how to set the role.'
+      );
+    }
   } catch (e) {
     log(e);
   }
