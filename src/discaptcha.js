bd50ca4b9564ec2b8cd4c8f264d9c3d16e846367
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-15 19:47:32 +0200

    Added proper handles for networking issues.

--- src/discaptcha.js
+++ src/discaptcha.js
@@ -66,67 +66,93 @@ const commandKeys = Object.keys(commands);
 const messageUtil = require('./utilities/util.message');
 const verifyUtil = require('./utilities/util.verify');
 const Discord = require('discord.js');
-
-// Login to Discord.
 const Client = new Discord.Client(config.clientOptions);
-Client.login(auth.token).catch(e => {
-  log('Failed to connect.', e);
+const reconnectTime = 10000;
+let reconnectTimeout;
+
+/**
+ * on.ready
+ * Triggers when the Client is ready to act,
+ * i.e. connected.
+ */
+Client.on('ready', () => {
+  log(
+    'Successfully connected to Discord 👏.\n' +
+      `Username: ${Client.user.username}\n` +
+      `Id: ${Client.user.id}\n` +
+      `Verified: ${Client.user.verified}\n` +
+      'Waiting for events...'
+  );
 });
 
 /**
- * Emitted When the Client is ready for action.
+ * on.message
+ * Triggers every time the bot sees a new message.
  */
-Client.on('ready', () => {
-  log('Successfully connected to Discord!');
-  log(
-    `Username: ${Client.user.username}, id: ${Client.user.id}, verified: ${
-      Client.user.verified
-    }.`
-  );
-  log('Waiting for events...');
-
-  /**
-   * on.message
-   * Triggers every time the bot sees a new message.
-   */
-  Client.on('message', Message => {
+Client.on('message', Message => {
+  if (
+    // Make sure the bot is mentioned.
+    Message.isMentioned(Client.user.id)
+  ) {
+    const c = messageUtil.readCommand(Message);
     if (
-      // Make sure the bot is mentioned.
-      Message.isMentioned(Client.user.id)
+      // Guild is required.
+      Message.guild &&
+      // Make sure the message contains a valid command.
+      commandKeys.includes(c.command) &&
+      // Make sure the client has the required permission.
+      messageUtil.hasPermission(
+        Message,
+        commands[c.command].permissions,
+        config.moderators,
+        auth.owner
+      )
     ) {
-      const c = messageUtil.readCommand(Message);
-      if (
-        // Guild is required.
-        Message.guild &&
-        // Make sure the message contains a valid command.
-        commandKeys.includes(c.command) &&
-        // Make sure the client has the required permission.
-        messageUtil.hasPermission(
-          Message,
-          commands[c.command].permissions,
-          config.moderators,
-          auth.owner
-        )
-      ) {
-        const command = commands[c.command];
-        log(`${Message.author.username} triggered ${c.command}.`);
-        if (command.mustVerify) {
-          // This command requires verifying.
-          const cmd = () => command.execute(Client, Message, c.value);
-          verifyUtil.verifyCommand(Message, command.verifyMessage, cmd);
-        } else {
-          // This command does not require verifying.
-          command.execute(Client, Message, c.value);
-        }
+      const command = commands[c.command];
+      log(`${Message.author.username} triggered ${c.command}.`);
+      if (command.mustVerify) {
+        // This command requires verifying.
+        const cmd = () => command.execute(Client, Message, c.value);
+        verifyUtil.verifyCommand(Message, command.verifyMessage, cmd);
+      } else {
+        // This command does not require verifying.
+        command.execute(Client, Message, c.value);
       }
     }
-  });
-
-  /**
-   * on.guildMemberAdd
-   * Triggers every time a new client joins the server (guild).
-   */
-  Client.on('guildMemberAdd', GuildMember => {
-    verifyUtil.verifyClient(GuildMember);
-  });
+  }
 });
+
+/**
+ * on.guildMemberAdd
+ * Triggers every time a new client joins the server (guild).
+ */
+Client.on('guildMemberAdd', GuildMember => {
+  verifyUtil.verifyClient(GuildMember);
+});
+
+/**
+ * on.reconnecting
+ * Triggered when Discord.js tries to reconnect after lost of
+ * Discord-connection.
+ */
+Client.on('reconnecting', () => {
+  log('Lost connection. Attempting to reconnect...');
+});
+
+/**
+ * on.error
+ * Emitted whenever the client's WebSocket encounters a connection error.
+ */
+Client.on('error', () => {
+  log(
+    `Discord.js websocket encountered a connection error.
+        Attempting to reconnect in ${reconnectTime / 1000} seconds.`
+  );
+  clearTimeout(reconnectTimeout);
+  reconnectTimeout = setTimeout(() => {
+    Client.login(auth.token);
+  }, reconnectTime);
+});
+
+// Here we go!
+Client.login(auth.token);
