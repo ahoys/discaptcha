0de2ba749b5edd14d44a113c7f6852a6255401db
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-16 17:10:33 +0200

    Improved verified strings.

--- src/commands/command.verified.js
+++ src/commands/command.verified.js
@@ -12,14 +12,44 @@ module.exports = (Client, Message, value = '') => {
         .getGuildMembersWithOrWithoutRole(Message.guild, true, Role)
         .then(GuildMembers => {
           const size = GuildMembers.size;
-          Message.reply(
-            `There are ${size} client(s) with the ${Role.name} -role.`
-          );
+          if (size < 1) {
+            // No verified.
+            Message.reply(
+              'There are no verified clients. ' +
+                `Use \`humanize\` to verify the current server members. ` +
+                `Humanize will give the role "${Role.name}" ` +
+                'to every member of this server, including those ' +
+                'who are offline.'
+            );
+          } else if (size === 1) {
+            // One verified.
+            Message.reply(
+              `Only ${GuildMembers.array().map(m => m.user.username)} ` +
+                'is verified.'
+            );
+          } else if (size <= 10) {
+            // Ten or less verified.
+            Message.reply(
+              `The following ${size} clients are verified:` +
+                `${GuildMembers.array().map(m => ' ' + m.user.username)}.`
+            );
+          } else {
+            // Over ten verified.
+            Message.reply(
+              `There are ${size} clients with the verified ` +
+                `"${Role.name}" -role.`
+            );
+          }
         })
         .catch(e => {
           log(e);
           Message.reply('execution failed.');
         });
+    } else {
+      Message.reply(
+        'The verified role is not set. ',
+        `Use command \`role\` to set the role. See \`help\` for more info.`
+      );
     }
   } catch (e) {
     log(e);
